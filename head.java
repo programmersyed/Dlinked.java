11e25fd7a8789785ad9f313959db30df4e7b9d21
programmersyed <abrahamsyed10@gmail.com>
2022-06-20 18:25:25 -0400

    Create head.java

---
+++ head.java
@@ -0,0 +1,15 @@
+public class DoublyLinkedList {
+    
+  public Node head;
+  public Node tail;
+
+  public DoublyLinkedList() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  public static void main(String[] args) {
+    
+  }
+
+}
